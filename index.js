f5fa3f554d240d0ebfc1ae69463786f1e1ea14cb
elisheva1885 <elisheva1885.gmail.com>
2024-12-27 02:30:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const func = () => {
+    console.log("happy Chanoka");
+}
+
+
+const func1 = ()=> {
+    alert("have a nice vocation")
+}
+
+const func2 = () => {
+    console.log("d'ont forget to eat a doughnut");
+}
+
+const func3 = () => {
+    alert("d'ont forget to visit your family")
+}
